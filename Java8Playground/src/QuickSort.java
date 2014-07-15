20791550d25e3eadc429837639236e8982450dee
Daniel Wolfe <dwolfe@zhobli33411.hob.wiley.com>
2014-07-15 11:18:57 +0100

    this time with the actual quicksort

--- Java8Playground/src/QuickSort.java
+++ Java8Playground/src/QuickSort.java
@@ -1,27 +1,43 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class QuickSort {
 
+	private static final int howManyNumbers = 2000000;
+
 	public static void main(String[] args) {
 		// if (args.length != 1) {
 		// System.out.println("Please enter exactly one argument");
 		// }
-		List<Integer> numbers = new ArrayList<Integer>(Arrays.asList(1, 344, 4,
-				3, 6, 2, 54, 67, 2, 4, 8, 90));
-		System.out.println(QuickSort.quickSort(numbers));// note the toString
-															// method does not
-															// pring out the
-															// memory address?!
+		long startTime = System.currentTimeMillis();
+		List<Integer> numbers = generateListOfRandomNumbers(howManyNumbers);
+		System.out.println("Created " + howManyNumbers + " Random Numbers: "
+				+ (System.currentTimeMillis() - startTime) + " milliseconds");
+		startTime = System.currentTimeMillis();
+		QuickSort.quickSort(numbers);// note the toString
+		// method does not
+		// pring out the
+		// memory address?!
+		System.out.println("Completed: "
+				+ (System.currentTimeMillis() - startTime) + " milliseconds");
+	}
+
+	private static List<Integer> generateListOfRandomNumbers(int howManyRandoms) {
+		List<Integer> numbers = new ArrayList<Integer>();
+		Random random = new Random();
+		for (int x = 0; x < howManyRandoms; x++) {
+			numbers.add(random.nextInt(howManyRandoms));
+		}
+		return numbers;
 	}
 
 	public static List<Integer> quickSort(List<Integer> numbers) {
 		if (numbers.size() <= 1)// exit if the size is 1 or empty because a list
-								// of size
-								// 1 is in sorted order
+			// of size
+			// 1 is in sorted order
 			return numbers;
 		// choose a pivot this is a non intelligent way of choosing a pivot.
 		Integer myPivot = numbers.remove(0);
@@ -34,6 +50,17 @@ public class QuickSort {
 
 	public static List<Integer> pivot(List<Integer> numbers,
 			Predicate<Integer> predicate) {
+		// return pivotNoJava8Streams(numbers, predicate);
 		return numbers.stream().filter(predicate).collect(Collectors.toList());
+	}
+
+	public static List<Integer> pivotNoJava8Streams(List<Integer> numbers,
+			Predicate<Integer> predicate) {
+		List<Integer> pivotList = new ArrayList<Integer>();
+		for (Integer i : numbers) {
+			if (predicate.test(i))
+				pivotList.add(i);
+		}
+		return pivotList;
 	}
 }
