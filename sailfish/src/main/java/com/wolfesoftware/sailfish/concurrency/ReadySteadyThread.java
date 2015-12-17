d2f314cbe8562bc4ffaa247ab4df04dc6c3a073f
Wolfe <dwolfe@WileyServices-MacBook-Pro-2.local>
2015-12-17 15:36:59 +0000

    KISS:made the threading code much more simpler

--- sailfish/src/main/java/com/wolfesoftware/sailfish/concurrency/ReadySteadyThread.java
+++ sailfish/src/main/java/com/wolfesoftware/sailfish/concurrency/ReadySteadyThread.java
@@ -8,14 +8,14 @@ import com.wolfesoftware.sailfish.concurrency.worker.factory.WorkerFactory;
 
 public class ReadySteadyThread {
 
-	private int numberOfThreads;
 	private WorkerFactory workerFactory;
 	long startTime;
 	ThreadPoolExecutor executor;
 
-	public ReadySteadyThread(int i, WorkerFactory threadFactory) {
-		this.numberOfThreads = i;
+	public ReadySteadyThread(int numberOfThreads, WorkerFactory threadFactory) {
 		this.workerFactory = threadFactory;
+		executor = new ThreadPoolExecutor(numberOfThreads, numberOfThreads, 0L,
+				TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>());
 	}
 
 	public void go() {
@@ -23,19 +23,22 @@ public class ReadySteadyThread {
 		this.execute();
 	}
 
-	/* this keeps adding runnable jobs from a workerfactory until the workerfactory indicates there is no more work to do*/
+	/*
+	 * this keeps adding runnable jobs from a workerfactory until the
+	 * workerfactory indicates there is no more work to do
+	 */
 	private void execute() {
-		executor = new ThreadPoolExecutor(numberOfThreads, numberOfThreads, 0L,
-				TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>());
-		while (!executor.isTerminated()) {
+		boolean shallIKeepGoing = true;
+		while (shallIKeepGoing) {
 			if (weHaveAnAvailableThread(executor)) {
-				if (!workerFactory.isThereAnyMoreWorkToDo()) {//race condition here 
-					executor.shutdown();
-				} else {
+				if (workerFactory.isThereAnyMoreWorkToDo()) {
 					addJobToExecutor();
+				} else {
+					shallIKeepGoing = false;
 				}
 			}
 		}
+		executor.shutdown();
 		System.out.println("ReadySteadyThreadCompleted"
 				+ (System.currentTimeMillis() - startTime));
 	}
