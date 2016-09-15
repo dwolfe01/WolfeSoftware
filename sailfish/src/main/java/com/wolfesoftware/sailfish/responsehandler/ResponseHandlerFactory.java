9e2856f1a640c89701f425e373fae0fb6a9d64f7
Wolfe <dwolfe@zeallidwolfe.local>
2016-09-15 10:17:06 +0100

    better factory method for response handling

--- sailfish/src/main/java/com/wolfesoftware/sailfish/responsehandler/ResponseHandlerFactory.java
+++ sailfish/src/main/java/com/wolfesoftware/sailfish/responsehandler/ResponseHandlerFactory.java
@@ -2,83 +2,55 @@ package com.wolfesoftware.sailfish.responsehandler;
 
 import java.io.OutputStream;
 
-import javax.management.RuntimeErrorException;
-
 import org.apache.http.StatusLine;
 import org.apache.http.client.ResponseHandler;
 
+import com.wolfesoftware.sailfish.responsehandler.ResponseHandlerFactory.ResponseHandlers;
+
 public class ResponseHandlerFactory {
 
-	public static ResponseHandlers currentResponseHandler = ResponseHandlers.DEFAULT;
-
-	public static void setHandler(ResponseHandlers handler) {
-		currentResponseHandler = handler;
-	}
-
-	public static ResponseHandler<StatusLine> getInstanceOfResponseHandler() {
-		return currentResponseHandler.getNewHandler();
-	}
+	private static ResponseHandlers handler = ResponseHandlers.DEFAULT;
 
 	public enum ResponseHandlers {
-		DEFAULT() {
-			@Override
-			public ResponseHandler<StatusLine> getNewHandler() {
-				return new QuickCloseResponseHandler();
-			}
-		},
-		SYSTEMOUT() {
-			@Override
-			public ResponseHandler<StatusLine> getNewHandler() {
-				return new SystemOutResponseHandler();
-			}
-		},
-		QUICKCLOSE() {
-			@Override
-			public ResponseHandler<StatusLine> getNewHandler() {
-				return new QuickCloseResponseHandler();
-			}
-		},
-		OUTPUTSTREAM {
-			OutputStream os = null;
-
-			public void setOutputStream(OutputStream os) {
-				this.os = os;
-			}
-
-			@Override
-			public ResponseHandler<StatusLine> getNewHandler() {
-				if (os == null) {// TODO:throw exception in this case
-					return new SystemOutResponseHandler();
-				} else {
-					return new OutputStreamResponseHandler(os);
-				}
-			}
-		},
-		PRINTHEADERS {
-			OutputStream os = null;
-
-			public void setOutputStream(OutputStream os) {
-				this.os = os;
-			}
-
-			@Override
-			public ResponseHandler<StatusLine> getNewHandler() {
-				if (os == null) {// TODO:throw exception in this case
-					throw new RuntimeErrorException(null, "You must provide an output stream for this handler");
-				} else {
-					return new PrintHeadersResponseHandler(os);
-				}
-			}
-		}
-		;
-
-		public abstract ResponseHandler<StatusLine> getNewHandler();
-
-		public void setOutputStream(OutputStream os)
-				throws NoSuchMethodException {
-			throw new NoSuchMethodException();
-		};
-
+		DEFAULT,
+		SYSTEMOUT,
+		QUICKCLOSE,
+		OUTPUTSTREAM,
+		PRINTHEADERS ;
 	};
+	
+	public static OutputStream outputStream = System.out;
+	
+	public static void setOutputStream(OutputStream os){
+		outputStream = os;
+	}
+	
+	public static OutputStream getOutputStream(){
+		return outputStream;
+	}
+	
+	public static ResponseHandler<StatusLine> getInstanceOfResponseHandler() {
+		return getInstanceOfResponseHandler(handler);
+	}
+	
+	public static ResponseHandler<StatusLine> getInstanceOfResponseHandler(ResponseHandlers handler) {
+		switch (handler){
+		 case DEFAULT:  
+			 return new QuickCloseResponseHandler();
+		 case SYSTEMOUT:
+			 return new SystemOutResponseHandler();
+		 case QUICKCLOSE:
+			 return new QuickCloseResponseHandler();
+		 case OUTPUTSTREAM:
+			 return new OutputStreamResponseHandler(getOutputStream());
+		 case PRINTHEADERS:
+			 return new PrintHeadersResponseHandler(getOutputStream());
+		 default:  
+			 return new QuickCloseResponseHandler();
+		}
+	}
 
+	public static void setHandler(ResponseHandlers handler) {
+		ResponseHandlerFactory.handler = handler;
+	}
 }
