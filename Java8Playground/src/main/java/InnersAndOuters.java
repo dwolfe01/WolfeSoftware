f957a7fb2b47c30e82978f4c9194fe75959d95ae
Wolfe <dwolfe01@hotmail.co.uk>
2017-05-03 15:22:56 +0100

    InnersAndOuter classes example- just a bit of fun

---
+++ Java8Playground/src/main/java/InnersAndOuters.java
@@ -0,0 +1,35 @@
+
+public class InnersAndOuters {
+
+	private String myName = "Outer";
+	public Inner inner;
+	
+	public class Inner {
+		public void seeOuter() {
+			System.out.println("FROM INNER myName :" + myName);
+			printMyName();
+			InnersAndOuters.this.printMyName();
+		}
+		
+		public void printMyName(){
+			System.out.println("FROM INNER printMyName myName :" + myName);
+		}
+	}
+	
+	public InnersAndOuters(){
+		inner = new Inner();
+	}
+	
+	public void printMyName(){
+		System.out.println("FROM OUTER printMyName myName :" + myName);
+	}
+	
+
+	public static void main(String[] args){
+		InnersAndOuters innersAndOuters = new InnersAndOuters();
+		innersAndOuters.inner.seeOuter();
+		System.out.println("*****");
+		Inner inner = innersAndOuters.new Inner();
+		inner.seeOuter();
+	}
+}
