bf433713e256a260198fa8d704fedd2f9f9c6a6b
Daniel Wolfe <dwolfe@wiley.com>
2018-02-14 12:20:58 +0000

    dwolfe|java 8 but no parallelism yet on prime finder

--- Java8Playground/src/main/java/PrimeFinder.java
+++ Java8Playground/src/main/java/PrimeFinder.java
@@ -13,6 +13,7 @@ public class PrimeFinder {
 	private BigInteger TWO = BigInteger.ONE.add(BigInteger.ONE);
 	private BigInteger myBigPrime = new BigInteger("982451653");
 	//private BigInteger myBigPrime = new BigInteger("15485864");//NOT A PRIME
+	//private BigInteger myBigPrime = new BigInteger("15485");//NOT A PRIME
 //	private BigInteger myBigPrime = new BigInteger("67280421310721");
 
 	public static void main(String[] args) {
@@ -26,6 +27,9 @@ public class PrimeFinder {
 //		System.out.println("SEQUENTIAL Is Prime: " + myBigPrime + " " + isPrimeNumber(myBigPrime));
 //		System.out.println(System.currentTimeMillis() - startTime + " millseconds");
 		startTime =  System.currentTimeMillis();
+		System.out.println("SEQUENTIAL Is Prime: " + myBigPrime + " " + isPrimeNumber(myBigPrime));
+		System.out.println(System.currentTimeMillis() - startTime + " millseconds");
+		startTime =  System.currentTimeMillis();
 		System.out.println("PARALLEL Is Prime: " + myBigPrime + " " + isPrimeNumberParallel(myBigPrime));
 		System.out.println(System.currentTimeMillis() - startTime + " millseconds");
 	}
@@ -33,13 +37,9 @@ public class PrimeFinder {
 	private boolean isPrimeNumberParallel(BigInteger primeCandidate) {
 		//get a stream of potential divisors
 		int takeWhile = primeCandidate.divide(TWO).intValue();
-		Stream.iterate(1L, n  ->  n  + 1).limit(491225826).collect(Collectors.toList());
-//		List<BigInteger> collect = Stream.iterate(bi("2"), i -> i.add(BigInteger.ONE)).limit(takeWhile).collect(Collectors.toList());
-		//List<Long> collect = Stream.iterate(1L, n  ->  n  + 1).limit(takeWhile).collect(Collectors.toList());
-//		Stream<List<BigInteger>> myNumbers = Stream.of(collect);
-		//myNumbers.parallel().forEach(System.out::println);
-		//boolean isPrime = divisors.parallel().noneMatch(i -> doesDivide(i, primeCandidate));
-		return false;
+		Stream<BigInteger> divisors = Stream.iterate(bi("2"), i -> i.add(BigInteger.ONE)).limit(takeWhile);
+		boolean isPrime = divisors.noneMatch(i -> doesDivide(i, primeCandidate));
+		return isPrime;
 	}
 
 	private boolean doesDivide(BigInteger divisor, BigInteger dividend){
