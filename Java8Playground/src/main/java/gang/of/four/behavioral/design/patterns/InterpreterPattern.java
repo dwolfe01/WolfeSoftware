4b12edddce7e72e6ee22b4474d45d97cc9f58c05
Daniel Wolfe <dwolfe@wiley.com>
2018-03-12 15:51:21 +0000

    better, more functional, Interpreter pattern

--- Java8Playground/src/main/java/gang/of/four/behavioral/design/patterns/InterpreterPattern.java
+++ Java8Playground/src/main/java/gang/of/four/behavioral/design/patterns/InterpreterPattern.java
@@ -4,7 +4,8 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Arrays;
+import java.util.function.Function;
+import java.util.function.Supplier;
 import java.util.stream.Stream;
 
 public class InterpreterPattern {
@@ -15,81 +16,55 @@ public class InterpreterPattern {
 			this.path = Paths.get("src/test/resources/story.txt");
 		}
 		
-		public Stream<String> getLinesAsStream() throws IOException {
-			return Files.lines(path);
+		public Stream<String> getLinesAsStream() {
+			try {
+				return Files.lines(path);
+			} catch (IOException e) {
+				throw new RuntimeException("Not able to read lines form the file");
+			}
 		}
 	}
 
 	abstract class Expression {
-		abstract boolean interpret(String string);
+		protected Function<String,Boolean> myFunction;
+		boolean interpret(String string) {
+			return myFunction.apply(string);
+		}
 	}
 	
 	class NotExpression extends Expression {
-		private Expression expression;
-
 		public NotExpression(Expression expression) {
-			this.expression = expression;
+			this.myFunction = s -> !expression.interpret(s);
 		}
-
-		boolean interpret(String str) {
-			return !this.expression.interpret(str);
-		}
-		
 	}
 
 	class WordExpression extends Expression {
-		private String word;
-
 		public WordExpression(String word) {
-			this.word = word;
-		}
-
-		public boolean interpret(String wordFromContext) {
-			return wordFromContext.contains(word);
+			this.myFunction = s -> s.contains(word);
 		}
 	}
 
 	class OrExpression extends Expression {
-		private Expression expression1 = null;
-		private Expression expression2 = null;
-
 		public OrExpression(Expression expression1, Expression expression2) {
-			this.expression1 = expression1;
-			this.expression2 = expression2;
-		}
-
-		public boolean interpret(String str) {
-			return expression1.interpret(str) || expression2.interpret(str);
+			this.myFunction = s ->expression1.interpret(s) || expression2.interpret(s);
 		}
 	}
 
 	class AndExpression extends Expression {
-
-		private Expression expression1;
-		private Expression expression2;
-
 		public AndExpression(Expression expression1, Expression expression2) {
-			this.expression1 = expression1;
-			this.expression2 = expression2;
-		}
-
-		public boolean interpret(String str) {
-			return expression1.interpret(str) && expression2.interpret(str);
+			this.myFunction = s ->expression1.interpret(s) && expression2.interpret(s);
 		}
 	}
 
 	class Interpreter {
-		private Context context;
-		private Expression expression;
-
+		protected Supplier<Stream<String>> myFunction;
+		
 		public Interpreter(Context context, Expression expression) {
-			this.context = context;
-			this.expression = expression;
+			myFunction = () -> context.getLinesAsStream().filter(s -> expression.interpret(s));
 		}
 		
-		public Stream<String> interpret() throws IOException {
-			//return context.getLinesAsStream();
-			return context.getLinesAsStream().filter(s -> expression.interpret(s));
+		public Stream<String> interpret() {
+			return myFunction.get();
 		}
 	}
 
@@ -99,9 +74,9 @@ public class InterpreterPattern {
 		OrExpression orExpression = ip.new OrExpression(ip.new WordExpression("Romeo"), ip.new WordExpression("Juliet"));
 		ip.new Interpreter(ip.new Context(),orExpression).interpret().count();
 		AndExpression andExpression = ip.new AndExpression(ip.new WordExpression("Romeo"), ip.new WordExpression("Juliet"));
-		ip.new Interpreter(ip.new Context(), andExpression).interpret().forEach(System.out::println);;
-		NotExpression notExpression = ip.new NotExpression(ip.new WordExpression("Romeo"));
-		ip.new Interpreter(ip.new Context(), notExpression).interpret().forEach(System.out::println);;
+		ip.new Interpreter(ip.new Context(), andExpression).interpret().forEach(System.out::println);
+		NotExpression notExpression = ip.new NotExpression(ip.new WordExpression("SERVICE"));
+		ip.new Interpreter(ip.new Context(), notExpression).interpret().forEach(System.out::println);
 	}
 
 }
