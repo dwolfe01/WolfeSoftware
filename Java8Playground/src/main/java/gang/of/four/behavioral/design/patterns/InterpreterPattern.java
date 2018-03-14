f4b1f138310fde4aadc42e9da58bd3073a921261
Daniel Wolfe <dwolfe@wiley.com>
2018-03-14 09:59:32 +0000

    method ordering changed

--- Java8Playground/src/main/java/gang/of/four/behavioral/design/patterns/InterpreterPattern.java
+++ Java8Playground/src/main/java/gang/of/four/behavioral/design/patterns/InterpreterPattern.java
@@ -9,20 +9,16 @@ import java.util.function.Supplier;
 import java.util.stream.Stream;
 
 public class InterpreterPattern {
-
-	class Context {
-		Path path;
-		public Context() throws IOException {
-			this.path = Paths.get("src/test/resources/story.txt");
-		}
-		
-		public Stream<String> getLinesAsStream() {
-			try {
-				return Files.lines(path);
-			} catch (IOException e) {
-				throw new RuntimeException("Not able to read lines from the file");
-			}
-		}
+	
+	public static void main(String[] args) throws IOException {
+		InterpreterPattern ip = new InterpreterPattern();
+		//the expressions is a grammar which is interpreted into a search
+		OrExpression orExpression = ip.new OrExpression(ip.new WordExpression("Romeo"), ip.new WordExpression("Juliet"));
+		ip.new Interpreter(ip.new Context(),orExpression).interpret().count();
+		AndExpression andExpression = ip.new AndExpression(ip.new WordExpression("Romeo"), ip.new WordExpression("Juliet"));
+		ip.new Interpreter(ip.new Context(), andExpression).interpret().forEach(System.out::println);
+		NotExpression notExpression = ip.new NotExpression(ip.new WordExpression("SERVICE"));
+		ip.new Interpreter(ip.new Context(), notExpression).interpret().forEach(System.out::println);
 	}
 
 	abstract class Expression {
@@ -67,16 +63,22 @@ public class InterpreterPattern {
 			return myFunction.get();
 		}
 	}
-
-	public static void main(String[] args) throws IOException {
-		InterpreterPattern ip = new InterpreterPattern();
-		//the expressions is a grammar which is interpreted into a search
-		OrExpression orExpression = ip.new OrExpression(ip.new WordExpression("Romeo"), ip.new WordExpression("Juliet"));
-		ip.new Interpreter(ip.new Context(),orExpression).interpret().count();
-		AndExpression andExpression = ip.new AndExpression(ip.new WordExpression("Romeo"), ip.new WordExpression("Juliet"));
-		ip.new Interpreter(ip.new Context(), andExpression).interpret().forEach(System.out::println);
-		NotExpression notExpression = ip.new NotExpression(ip.new WordExpression("SERVICE"));
-		ip.new Interpreter(ip.new Context(), notExpression).interpret().forEach(System.out::println);
+	
+	class Context {
+		Path path;
+		public Context() throws IOException {
+			this.path = Paths.get("src/test/resources/story.txt");
+		}
+		
+		public Stream<String> getLinesAsStream() {
+			try {
+				return Files.lines(path);
+			} catch (IOException e) {
+				throw new RuntimeException("Not able to read lines from the file");
+			}
+		}
 	}
+
+	
 
 }
