f35ea240f65319bc8230bc3e2a050224a5441014
Daniel Wolfe <dwolfe@wiley.com>
2018-06-07 15:10:02 +0100

    decorator pattern

--- Java8Playground/src/main/java/gang/of/four/structural/design/patterns/DecoratorPattern.java
+++ Java8Playground/src/main/java/gang/of/four/structural/design/patterns/DecoratorPattern.java
@@ -2,6 +2,32 @@ package gang.of.four.structural.design.patterns;
 
 public class DecoratorPattern {
 	
-	
+	class DecorateSimpleText{
+		private SimpleText text;
 
+		public DecorateSimpleText(SimpleText text) {
+			this.text = text;
+			
+		}
+
+		public String get() {
+			return "<h1>" + text.get()+"</h1>";
+		}
+	}
+	
+	class SimpleText{
+		String contents = "Hello World";
+
+		public String get() {
+			return contents;
+		}
+	}
+	
+	public static void main(String...args) {
+		DecoratorPattern dp = new DecoratorPattern();
+		SimpleText text = dp.new SimpleText();
+		System.out.println(text.get());
+		System.out.println(dp.new DecorateSimpleText(text).get());
+	}
+	
 }
